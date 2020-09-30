5c914255415270295642b287a4127439da131b55
ivvnv <ivana.rivera.c@gmail.com>
2020-09-29 21:33:23 -0300

    Updates

---
+++ precache-manifest.700af02f90ca95a85ccdde2fe2150bed.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da964b1d2e30ce549c948f02f6712d14",
+    "url": "/hackathon013/index.html"
+  },
+  {
+    "revision": "4daa5dfeac970873043e",
+    "url": "/hackathon013/static/css/main.64facb21.chunk.css"
+  },
+  {
+    "revision": "261e88d71d5a45f01d62",
+    "url": "/hackathon013/static/js/0.9e3f5016.chunk.js"
+  },
+  {
+    "revision": "add6bef4fedd8f5e853d38a08fb657d7",
+    "url": "/hackathon013/static/js/0.9e3f5016.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "460e8ce68d29f418747e",
+    "url": "/hackathon013/static/js/10.ec4eb926.chunk.js"
+  },
+  {
+    "revision": "d4af7d219571f98c326832f3f840d494",
+    "url": "/hackathon013/static/js/10.ec4eb926.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c29aff8ca28ca12b035",
+    "url": "/hackathon013/static/js/11.d480ddc4.chunk.js"
+  },
+  {
+    "revision": "6a8c74c96d4d40d5e87ab4543287bac3",
+    "url": "/hackathon013/static/js/11.d480ddc4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ad6e91cccb4c98b0e23",
+    "url": "/hackathon013/static/js/3.ccde4947.chunk.js"
+  },
+  {
+    "revision": "df29ce23dc71344ae95e2113fe8322a8",
+    "url": "/hackathon013/static/js/3.ccde4947.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "647a6f54c5ed0b221e9c",
+    "url": "/hackathon013/static/js/4.521b899c.chunk.js"
+  },
+  {
+    "revision": "7c1ad81c26e5f42b2a26d3a9edcf8073",
+    "url": "/hackathon013/static/js/4.521b899c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b12a23f042f7a3cc3c9",
+    "url": "/hackathon013/static/js/5.490043d0.chunk.js"
+  },
+  {
+    "revision": "7feb36c6c39f703ee09d65c567036575",
+    "url": "/hackathon013/static/js/5.490043d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1356ad3814492fb5917",
+    "url": "/hackathon013/static/js/6.12c0e128.chunk.js"
+  },
+  {
+    "revision": "6a8c74c96d4d40d5e87ab4543287bac3",
+    "url": "/hackathon013/static/js/6.12c0e128.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b9ff38dbd0e5163adfa",
+    "url": "/hackathon013/static/js/7.00530a11.chunk.js"
+  },
+  {
+    "revision": "add6bef4fedd8f5e853d38a08fb657d7",
+    "url": "/hackathon013/static/js/7.00530a11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2f5a81914b6cdf8ad61",
+    "url": "/hackathon013/static/js/8.f22a498f.chunk.js"
+  },
+  {
+    "revision": "add6bef4fedd8f5e853d38a08fb657d7",
+    "url": "/hackathon013/static/js/8.f22a498f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f83d844bd01f5c1aca43",
+    "url": "/hackathon013/static/js/9.fd78d428.chunk.js"
+  },
+  {
+    "revision": "8a59271068243bb3f9e53b336c61dba4",
+    "url": "/hackathon013/static/js/9.fd78d428.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4daa5dfeac970873043e",
+    "url": "/hackathon013/static/js/main.4b3c1dc6.chunk.js"
+  },
+  {
+    "revision": "a43c24db73c44ac246fd",
+    "url": "/hackathon013/static/js/runtime-main.8871807a.js"
+  },
+  {
+    "revision": "d8fa7989e8e27aa0f315be2aeae8a934",
+    "url": "/hackathon013/static/media/Bienestar.d8fa7989.svg"
+  },
+  {
+    "revision": "1cca744eee432a2759dd863636bc3702",
+    "url": "/hackathon013/static/media/GON.1cca744e.svg"
+  },
+  {
+    "revision": "6007b8b6a841832d74566066cb87b574",
+    "url": "/hackathon013/static/media/Logout.6007b8b6.svg"
+  },
+  {
+    "revision": "ce6d05b3db796939cc6e9df151162958",
+    "url": "/hackathon013/static/media/Metricas.ce6d05b3.svg"
+  },
+  {
+    "revision": "0e0a0ec249ee7a63790f6ddd3d390511",
+    "url": "/hackathon013/static/media/On.0e0a0ec2.svg"
+  },
+  {
+    "revision": "066715b0b2554753c9d5748ec0371ffd",
+    "url": "/hackathon013/static/media/Perfil.066715b0.svg"
+  },
+  {
+    "revision": "c4340710ebea1b15a632765f85dade75",
+    "url": "/hackathon013/static/media/Tareas.c4340710.svg"
+  },
+  {
+    "revision": "c3568a45d6216ee9ba758d6182cdc075",
+    "url": "/hackathon013/static/media/eylogo.c3568a45.svg"
+  },
+  {
+    "revision": "1009ecb48a634aaf472140f98968f0ca",
+    "url": "/hackathon013/static/media/logo.1009ecb4.png"
+  }
+]);
